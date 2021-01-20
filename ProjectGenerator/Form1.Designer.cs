0b92436da7a6449ae7bdec012cd5cf16d8147a41
Timoy_7207 <jostimian@gmail.com>
2021-01-20 10:50:19 +0800

    initial Commit

---
+++ ProjectGenerator/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ProjectGenerator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.darkLabel1 = new DarkUI.Controls.DarkLabel();
+			this.SuspendLayout();
+			// 
+			// darkLabel1
+			// 
+			this.darkLabel1.AutoSize = true;
+			this.darkLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+			this.darkLabel1.Location = new System.Drawing.Point(387, 199);
+			this.darkLabel1.Name = "darkLabel1";
+			this.darkLabel1.Size = new System.Drawing.Size(60, 13);
+			this.darkLabel1.TabIndex = 0;
+			this.darkLabel1.Text = "darkLabel1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.darkLabel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private DarkUI.Controls.DarkLabel darkLabel1;
+	}
+}
+
---
+++ ProjectGenerator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ProjectGenerator
+{
+	public partial class Form1 : Form
+	{
+		String genFilePath = @"C:\Users\josti\source\repos\ProjectGenerator\ProjectGenerator\Generator Files\gen.json";
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public void loadFile()
+		{
+			StreamReader reader = new StreamReader(genFilePath);
+			String genFileString = reader.ReadToEnd();
+			genFileSturct obj = JsonConvert.DeserializeObject<genFileSturct>(genFileString);
+			darkLabel1.Text = obj.Language;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			loadFile();
+		}
+	}
+	public class genFileSturct
+	{
+		public String Language { get; set; }
+		public bool GitCommit { get; set; }
+	}
+}
